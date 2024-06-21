972e2659128190357618e5a785444626d5e95d26
MachadoPiter <piter.machado@ofx.com>
2024-06-21 10:18:59 +1000

    Added wfs

--- GHA2/Service/CalcService.cs
+++ GHA2/Service/CalcService.cs
@@ -1,6 +1,24 @@
-﻿namespace GHA2.Service;
+﻿
+namespace GHA2.Service;
 
 public class CalcService : ICalcService
 {
     public int Add(int num1, int num2) => num1 + num2;
+
+    public int Subtract(int v1, int v2)
+    {
+        return v1 - v2;
+    }
+
+    // Write me division and multiplication methods 
+    public int Multiply(int v1, int v2)
+    {
+        return v1 * v2;
+    }
+
+    // Write division method
+    public int Divide(int v1, int v2)
+    {
+        return v1 / v2;
+    }
 }
